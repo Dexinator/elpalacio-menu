a1bead7e5990dd1535a88b9fa5217f072ca4fe89
Jorge Badillo <badillo.delgado.jorge@gmail.com>
2024-04-10 10:49:08 -0600

    reverted url change

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ const m2dxOptions = {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "Palacio Negro",
+  site: "https://nebulix.unfolding.io",
   integrations: [
     icon(),
     mdx({}),
